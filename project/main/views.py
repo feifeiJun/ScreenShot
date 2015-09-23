5c608a6fe32b5ee019000c52f9184c0f5cfc580e
yifei_zhou <zhouyifei@hotmail.com>
2015-09-23 10:27:10 +0800

    some changes

--- project/main/views.py
+++ project/main/views.py
@@ -4,15 +4,15 @@
 #################
 #### imports ####
 #################
-
 from flask import render_template, Blueprint
-
+import os
+import subprocess
 
 ################
 #### config ####
 ################
 
-main_blueprint = Blueprint('main', __name__,)
+main_blueprint = Blueprint('main', __name__, )
 
 
 ################
@@ -28,3 +28,11 @@ def home():
 @main_blueprint.route("/about/")
 def about():
     return render_template("main/about.html")
+
+
+@main_blueprint.route('/shoot/')
+def shoot():
+    mUrl = "www.hust.edu.cn"
+    path = os.path.dirname(os.path.realpath(__file__)) + "/shoot.py"
+    subprocess.check_output("python " + path + " " + mUrl, shell=True)
+    return 'hello'
