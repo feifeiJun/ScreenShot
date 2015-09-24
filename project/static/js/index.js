15daf1f650e8c01b728c3f72a1bc1a31da4b0fe5
李飞 <Fei@LMac.lan>
2015-09-24 14:53:22 +0800

    add slide

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,7 +1,34 @@
 /**
  * Created by Fei on 15/9/16.
  */
-var Submitt=document.getElementsByClassName("Submit")[0];
-Submitt.onclick=function(){
-    alert(" Fei")
+var Submit=document.getElementById("Submit");
+Submit.onclick=function(){
+    var Search=document.getElementById("Search");
+    var Logo=document.getElementById("Dlogo");
+    var WebSite=document.getElementById("WebSite");
+    var Capture=document.getElementById("ScreenShot");
+
+    Logo.style.float= "left";
+    Logo.style.margin= "0% 15%";
+    Logo.style.margin.right="5%";
+
+    Search.style.float= "left";
+    Search.style.margin="1.5% auto";
+
+    Capture.style.visible="visible";
+    //Capture.style.border="darkkhaki dashed";
+    //Capture.style.border.radius="5px";
+
+
+
+    $.ajax({
+            type: "get",
+            url: "",
+            data: WebSite.value,
+            success:function(data,staus){
+                $("#ScreenShot").css({"display":"block","backgroundImage":""});
+
+            }
+        }
+    )
 }
