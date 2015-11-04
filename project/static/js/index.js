3557ce331cefbee09dcc18b7f7650a125cb8eb32
李飞 <Fei@LMac.local>
2015-11-04 23:36:24 +0800

    add project

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -2,33 +2,41 @@
  * Created by Fei on 15/9/16.
  */
 var Submit=document.getElementById("Submit");
-Submit.onclick=function(){
-    var Search=document.getElementById("Search");
-    var Logo=document.getElementById("Dlogo");
-    var WebSite=document.getElementById("WebSite");
-    var Capture=document.getElementById("ScreenShot");
+Submit.onclick=function() {
+    var Search = document.getElementById("Search");
+    var Logo = document.getElementById("Dlogo");
+    var WebSite = document.getElementById("WebSite");
+    var Capture = document.getElementById("ScreenShot");
 
-    Logo.style.float= "left";
-    Logo.style.margin= "0% 15%";
-    Logo.style.margin.right="5%";
-
-    Search.style.float= "left";
-    Search.style.margin="1.5% auto";
-
-    Capture.style.visible="visible";
+    Logo.style.float = "left";
+    //Logo.style.margin = "0% 7% 0 19% ";
+    Logo.style.top = "5%";
+    Logo.style.left = "22%";
 
 
 
+    Search.style.float = "left";
+    Search.style.top = "6%";
+    Search.style.left= "25%";
+  
 
-    $.ajax({
-            type: "POST",
-            url: "/shoot/",
-            data: {url: WebSite.value},
-            success:function(data,staus){
-                url = data.url;
-                $("#ScreenShot").css({"display":"block","background-image": "url('" + url + "')"});
-
-            }
-        }
-    )
+    //Search.style.margin = "1.5% auto";
 }
+
+    Capture.style.display="block";
+
+
+
+
+//    $.ajax({
+//            type: "POST",
+//            url: "/shoot/",
+//            data: {url: WebSite.value},
+//            success:function(data,staus){
+//                url = data.url;
+//                $("#ScreenShot").css({"display":"block","background-image": "url('" + url + "')"});
+//
+//            }
+//        }
+//    )
+//}
